b191d419d9ba1039e348dd9b24388ec183fe9b02
Dominik Schidlowski <dominik@schidlowski.eu>
2021-02-25 00:52:42 +0100

    usage of middleware

--- http/response.go
+++ http/response.go
@@ -5,3 +5,15 @@ type Response struct {
 	Error   string      `json:"error"`
 	Data    interface{} `json:"data"`
 }
+
+func (receiver *Response) Ok(data interface{}) {
+	receiver.Success = true
+	receiver.Error = ""
+	receiver.Data = data
+}
+
+func (receiver *Response) Err(err error) {
+	receiver.Success = false
+	receiver.Error = err.Error()
+	receiver.Data = nil
+}
--- http/server.go
+++ http/server.go
@@ -1,6 +1,9 @@
 package http
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"github.com/schidstorm/sshd-manager/manager"
 	"github.com/schidstorm/sshd-manager/parser"
 	"github.com/schidstorm/sshd-manager/putter"
@@ -22,82 +25,106 @@ func NewServer(addr string, putterObj putter.Putter) *Server {
 }
 
 func (server *Server) Run(addr string) error {
-	http.HandleFunc("/account", server.handleAccount)
-	http.HandleFunc("/server", server.handleServer)
-	http.HandleFunc("/serverKeys", server.handleServerKeys)
+	http.Handle("/account", jsonOutputMiddleware(http.HandlerFunc(server.handleAccount)))
+	http.Handle("/server", jsonOutputMiddleware(http.HandlerFunc(server.handleServer)))
+	http.Handle("/serverKeys", jsonOutputMiddleware(http.HandlerFunc(server.handleServerKeys)))
 
 	logrus.Infof("listening on %s", addr)
 	return http.ListenAndServe(addr, nil)
 }
 
 func (server *Server) handleServerKeys(writer http.ResponseWriter, request *http.Request) {
+	response := request.Context().Value("response").(*Response)
 	err := server.putterObj.Connect()
 	defer server.putterObj.Disconnect()
 	if err != nil {
-		panic(err)
-	}
-	if request.Method == "GET" {
-		serverKey := request.URL.Query().Get("key")
-		publicKeys := server.putterObj.GetPublicKeysByServerKey(request.Context(), serverKey)
-
-		buffer, _ := parser.SerializeJson(publicKeys)
-		writer.WriteHeader(200)
-		writer.Write(buffer)
+		response.Err(err)
 		return
 	}
 
-	writer.WriteHeader(500)
-	writer.Write([]byte("Failed"))
+	if request.Method == "GET" {
+		serverKey := request.URL.Query().Get("key")
+		publicKeys := server.putterObj.GetPublicKeysByServerKey(request.Context(), serverKey)
+		response.Ok(publicKeys)
+	} else {
+		response.Err(errors.New("method not allowed"))
+	}
 }
 
 func (server *Server) handleServer(writer http.ResponseWriter, request *http.Request) {
+	response := request.Context().Value("response").(*Response)
 	defer request.Body.Close()
 	err := server.putterObj.Connect()
 	defer server.putterObj.Disconnect()
 	if err != nil {
-		panic(err)
+		response.Err(err)
+		return
 	}
+
 	if request.Method == "POST" {
 		managedServer := &manager.Server{}
 		buffer, _ := ioutil.ReadAll(request.Body)
 		err := parser.ParseYaml(managedServer, string(buffer))
 		if err != nil {
-			panic(err)
+			response.Err(err)
+			return
 		}
 
 		server.putterObj.AddServer(request.Context(), managedServer)
 
-		writer.WriteHeader(200)
-		writer.Write([]byte("OK"))
-		return
+		response.Ok(nil)
+	} else {
+		response.Err(errors.New("method not allowed"))
 	}
-
-	writer.WriteHeader(500)
-	writer.Write([]byte("Failed"))
 }
 
 func (server *Server) handleAccount(writer http.ResponseWriter, request *http.Request) {
+	response := request.Context().Value("response").(*Response)
 	defer request.Body.Close()
 	err := server.putterObj.Connect()
 	defer server.putterObj.Disconnect()
 	if err != nil {
-		panic(err)
+		response.Err(err)
+		return
 	}
+
 	if request.Method == "POST" {
 		account := &manager.Account{}
 		buffer, _ := ioutil.ReadAll(request.Body)
 		err := parser.ParseYaml(account, string(buffer))
 		if err != nil {
-			panic(err)
+			response.Err(err)
+			return
 		}
 
 		server.putterObj.AddAccount(request.Context(), account)
+		response.Ok(nil)
+	} else {
+		response.Err(errors.New("method not allowed"))
+	}
+}
 
+func jsonOutputMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		response := &Response{}
+		responseCtx := r.WithContext(context.WithValue(r.Context(), "response", response))
+		next.ServeHTTP(w, responseCtx)
+		writeResponse(w, response)
+	})
+}
+
+func writeResponse(writer http.ResponseWriter, response *Response) {
+	writer.Header().Set("Content-Type", "application/json")
+	if response.Success {
 		writer.WriteHeader(200)
-		writer.Write([]byte("OK"))
+	} else {
+		writer.WriteHeader(500)
+	}
+	jsonData, err := json.Marshal(*response)
+	if err != nil {
+		logrus.Error(err)
 		return
 	}
 
-	writer.WriteHeader(500)
-	writer.Write([]byte("Failed"))
+	writer.Write(jsonData)
 }
